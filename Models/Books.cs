07efcf2572df9407f3f23ccd90a1a7f0520319c1
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-25 16:59:35 -0500

    added Books model

---
+++ Models/Books.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Models
+{
+    public class Books
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Chapters { get; set; }
+        public DateTime YearOfPublish { get; set; }
+        public Boolean Completed { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
