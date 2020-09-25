93a1a83fe049c1ba8235af3bceefd4821be339f8
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-25 17:06:05 -0500

    added book model

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace capstone.Models
 {
-    public class Books
+    public class Book
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
