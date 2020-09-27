43dbbf043f14aa152943c9eae6ae797fe284092b
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-27 14:51:54 -0500

    Added Data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -28,5 +28,57 @@ namespace capstone.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>()
+            .HasData(
+                new Book
+                { 
+                    Id = 1,
+                    Title = "Prince Caspian", 
+                    Author = "C.S. Lewis", 
+                    Genre = "Fantasy", 
+                    Chapters = 15, 
+                    YearOfPublish = new DateTime(1951, 10, 15), 
+                    Completed = true,
+                    UserId = "2845a7e7-b0cc-4500-a672-10d476d09ac4"
+                },
+                new Book
+                { 
+                    Id = 2, 
+                    Title = "The Hobbit", 
+                    Author = "J.R.R. Tolkien", 
+                    Genre = "Fantasy", Chapters = 19, 
+                    YearOfPublish = new DateTime(1939, 9, 21), 
+                    Completed = true,
+                    UserId = "2845a7e7-b0cc-4500-a672-10d476d09ac4"
+                },
+                new Book 
+                { 
+                    Id = 3, 
+                    Title = "Starship Troopers", 
+                    Author = "Robert A. Heinlin", 
+                    Genre = "Sci-fi", 
+                    Chapters = 14, 
+                    YearOfPublish = new DateTime(1959, 11, 1), 
+                    Completed = true,
+                    UserId = "2845a7e7-b0cc-4500-a672-10d476d09ac4"
+                },
+                new Book 
+                { 
+                    Id = 4, 
+                    Title = "Radical", 
+                    Author = "Davic Platt", 
+                    Genre = "Christian", 
+                    Chapters = 9, 
+                    YearOfPublish = new DateTime(2010, 4, 25), 
+                    Completed = false,
+                    UserId = "2845a7e7-b0cc-4500-a672-10d476d09ac4"
+                }
+            );
+
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ Data/Migrations/20200927194235_BookData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace capstone.Data.Migrations
+{
+    public partial class BookData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Chapters", "Completed", "Genre", "Title", "UserId", "YearOfPublish" },
+                values: new object[] { 1, "C.S. Lewis", 15, true, "Fantasy", "Prince Caspian", "2845a7e7-b0cc-4500-a672-10d476d09ac4", new DateTime(1951, 10, 15, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Chapters", "Completed", "Genre", "Title", "UserId", "YearOfPublish" },
+                values: new object[] { 2, "J.R.R. Tolkien", 19, true, "Fantasy", "The Hobbit", "2845a7e7-b0cc-4500-a672-10d476d09ac4", new DateTime(1939, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Chapters", "Completed", "Genre", "Title", "UserId", "YearOfPublish" },
+                values: new object[] { 3, "Robert A. Heinlin", 14, true, "Sci-fi", "Starship Troopers", "2845a7e7-b0cc-4500-a672-10d476d09ac4", new DateTime(1959, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Chapters", "Completed", "Genre", "Title", "UserId", "YearOfPublish" },
+                values: new object[] { 4, "Davic Platt", 9, false, "Christian", "Radical", "2845a7e7-b0cc-4500-a672-10d476d09ac4", new DateTime(2010, 4, 25, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
