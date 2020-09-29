9272bf7e5cfcde8c218d99589a1d458ac4d95653
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-09-28 19:50:06 -0500

    completed book services

--- ClientApp/src/app/service/book-service.service.spec.ts
+++ ClientApp/src/app/service/book-service.service.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
-import { BookServiceService } from './book-service.service';
+import { BookService } from './book-service.service';
 
 describe('BookServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: BookServiceService = TestBed.get(BookServiceService);
+    const service: BookService = TestBed.get(BookService);
     expect(service).toBeTruthy();
   });
 });
--- ClientApp/src/app/service/book-service.service.ts
+++ ClientApp/src/app/service/book-service.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ibook } from '../interfaces/ibook';
+import { BookComponent } from '../book/book.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookServiceService {
+export class BookService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+  public async getBook(): Promise<Ibook[]> {
+    return this.httpClient.get<Ibook[]>('${this.baseUrl}book').toPromise();
+  }
+
+  public async addBook(book: Ibook): Promise<Ibook> {
+    return this.httpClient.post<Ibook>('${this.baseUrl}book', book).toPromise();
+  }
 }
